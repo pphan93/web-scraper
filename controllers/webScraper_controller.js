6e20554989bc8926d82180f9de5c657abac577d7
Phuoc phan <phuocphan12@gmail.com>
2019-06-01 00:03:49 -0400

    done

--- controllers/webScraper_controller.js
+++ controllers/webScraper_controller.js
@@ -12,24 +12,11 @@ mongoose.connect("mongodb://localhost/populatedb", {
     useNewUrlParser: true
 });
 
-// // Database configuration
-// var databaseUrl = "scraper";
-// var collections = ["scrapedData"];
-
-
-
 // Require all models
 var db = require("../models");
 
-// // Hook mongojs configuration to the db variable
-// var db = mongojs(databaseUrl, collections);
-// db.on("error", function (error) {
-//     console.log("Database Error:", error);
-// });
-
 // Main route (simple Hello World Message)
 router.get("/", function (req, res) {
-    //console.log("hello")
     // Find all results from the scrapedData collection in the db
     db.Article.find({
         saved: 0
@@ -40,33 +27,13 @@ router.get("/", function (req, res) {
         }
         // If there are no errors, send the data to the browser as json
         else {
-            //res.json(found);
-
-            //console.log(found)
             res.render("index", {
                 found: found
             });
         }
     });
-
-
 });
 
-// Retrieve data from the db
-router.get("/all", function (req, res) {
-    // Find all results from the scrapedData collection in the db
-    db.Article.find(function (error, found) {
-        // Throw any errors to the console
-        if (error) {
-            console.log(error);
-        }
-        // If there are no errors, send the data to the browser as json
-        else {
-            console.log("hello")
-            res.json(found);
-        }
-    });
-});
 
 router.post("/save", function (req, res) {
     var postId = req.body.id
@@ -89,6 +56,38 @@ router.post("/save", function (req, res) {
     })
 })
 
+router.post("/removeComment", function (req, res) {
+    db.Note.findOneAndRemove({
+        _id: req.body.id
+    }, function (err) {
+        if (err) throw err;
+        db.Article.findOneAndUpdate({
+                "comments": req.body.id
+            }, {
+                "$pull": {
+                    "comments": req.body.id
+                }
+            }, {
+                new: true
+            },
+            function (err, result) {
+                if (err) throw err;
+                console.log(result)
+                db.Article.findOne({
+                        _id: result._id
+                    })
+                    .populate("comments")
+                    .then(function (dbArticle) {
+                        res.json(dbArticle);
+                    })
+                    .catch(function (err) {
+                        res.json(err);
+                    });
+            }
+        );
+    });
+})
+
 router.post("/comment", function (req, res) {
     var postId = req.body.id
     var comment = req.body.comment
@@ -100,12 +99,8 @@ router.post("/comment", function (req, res) {
             name: name,
             comment: comment
         })
-        //console.log("test")
         .then(function (dbNote) {
             console.log(dbNote)
-            // If a Note was created successfully, find one User (there's only one) and push the new Note's _id to the User's `notes` array
-            // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-            // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
             return db.Article.findOneAndUpdate({
                 _id: postId
             }, {
@@ -117,8 +112,7 @@ router.post("/comment", function (req, res) {
             });
         })
         .then(function (dbArticle) {
-            // If the User was updated successfully, send it back to the client
-            //console.log(dbArticle._id)
+
 
             db.Article.findOne({
                     _id: dbArticle._id
@@ -151,9 +145,6 @@ router.get("/saved", function (req, res) {
         }
         // If there are no errors, send the data to the browser as json
         else {
-            //res.json(found);
-
-            //console.log(found)
             res.render("index", {
                 found: found
             });
@@ -172,10 +163,8 @@ router.get("/post", function (req, res) {
         // Specify that we want to populate the retrieved users with any associated notes
         .populate("comments")
         .then(function (result) {
-            // If able to successfully find and associate all Users and Notes, send them back to the client
-            //res.json(dbArticle);
 
-console.log(result)
+            console.log(result)
 
             axios.get(result.link).then(function (response) {
                 // Load the html body from axios into cheerio
@@ -192,36 +181,18 @@ console.log(result)
             // If an error occurs, send it back to the client
             res.json(err);
         });
-
-
-    // db.Article.findById(postId, function (error, found) {
-
-    //     if (error) {
-    //         console.log(error);
-    //     }
-    //     // If there are no errors, send the data to the browser as json
-    //     else {
-    //         console.log(found)
-    //         //res.json(found);
-
-    //         var result = found
-
-    //         axios.get(result.link).then(function (response) {
-    //             // Load the html body from axios into cheerio
-    //             var $ = cheerio.load(response.data);
-    //             $("#articlebody").each(function (i, element) {
-    //                 //console.log($(element).html())
-    //                 result.html = $(element).html()
-    //                 res.render("post", result)
-    //             });
-
-    //         })
-    //     }
-    // });
-
-
-
 });
+
+router.get("/clear", function (req, res) {
+    console.log("Test")
+    db.Article.deleteMany({}, function (error, result) {
+
+        db.Note.deleteMany({}, function (err, result) {
+            res.redirect("/")
+        })
+    })
+
+})
 
 // Scrape data from one site and place it into the mongodb db
 router.get("/scrape", function (req, res) {
--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -32,54 +32,10 @@ $("#comment").click(function (e) {
         function (data) {
             console.log(data)
             var comments = data.comments
-
-            var commentSection = $("#commentSection")
-            commentSection.empty();
-            var html;
-
-            for (var i = 0; i < comments.length; i++) {
-                var currComment = comments[i].comment
-                var date = new Date(comments[i].date).toISOString().replace(/T/, " ").replace(/\..+/, '').replace(/\d\d:\d\d:\d\d/, '')
-                var name = comments[i].name
-                var id = comments[i]._id
-                html = '</br><strong class="pull-left primary-font">' + name + '</strong> <small class="pull-right text-muted"><span class="glyphicon glyphicon-time"></span>'
-                html = html + date+ '<a class="removeComment float-right" href="#" data-id='+ id + '><i class="fas fa-times"></i></a></small>'+ '</small></br><li class="ui-state-default">' + currComment
-                html = html + '</li>'
-                commentSection.append(html)
-
-
-
-                // </br>
-                // <strong class="pull-left primary-font">James</strong>
-                // <small class="pull-right text-muted">
-                //     <span class="glyphicon glyphicon-time"></span>7 mins ago</small>
-                // </br>
-                // <li class="ui-state-default">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                //     tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                //     exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </li>
-
-            }
+            outputComment(comments)
         });
 });
 
-// $("a.postLink").click(function (e) {
-//     e.preventDefault();
-//     // write your code
-//     //console.log($(this).data("id"))
-
-//     var id = $(this).data("id")
-
-
-
-//     // Create an object for the user"s data
-//     var userData = {
-//         id: $(this).data("id")
-//     };
-
-//     console.log(userData)
-
-//     window.location = ("/post?id=" + id +"#comment");
-// });
 
 $("a.saveArticle").click(function (e) {
     e.preventDefault();
@@ -105,14 +61,44 @@ $("a.saveArticle").click(function (e) {
         })
     }
 
-
-    // $.get(
-    //     "/scrape",
-    //     function (data) {
-    //         var $errorMsg = $("#alertMsg").addClass("alert alert-info text-center").text(data);
-    //         setTimeout(function () {
-    //             $errorMsg.removeClass("alert alert-info").empty();
-    //             window.location.replace("/");
-    //         }, 2000)
-    //     });
 });
+
+
+$(document).on("click","a.removeComment",function (e) {
+    e.preventDefault();
+    // write your code
+    console.log("remove")
+
+    var userData = {
+        id: $(this).data("id")
+    };
+
+
+        $.post("/removeComment", userData, function (data) {
+            console.log(data)
+            var comments = data.comments
+            outputComment(comments)
+        })
+    
+
+});
+
+
+
+function outputComment(comments) {
+
+    var commentSection = $("#commentSection")
+    commentSection.empty();
+    var html;
+
+    for (var i = 0; i < comments.length; i++) {
+        var currComment = comments[i].comment
+        var date = new Date(comments[i].date).toISOString().replace(/T/, " ").replace(/\..+/, '').replace(/\d\d:\d\d:\d\d/, '')
+        var name = comments[i].name
+        var id = comments[i]._id
+        html = '</br><strong class="pull-left primary-font">' + name + '</strong> <small class="pull-right text-muted"><span class="glyphicon glyphicon-time"></span>'
+        html = html + date + '<a class="removeComment float-right" href="#" data-id=' + id + '><i class="fas fa-times"></i></a></small>' + '</small></br><li class="ui-state-default">' + currComment
+        html = html + '</li>'
+        commentSection.append(html)
+    }
+}
